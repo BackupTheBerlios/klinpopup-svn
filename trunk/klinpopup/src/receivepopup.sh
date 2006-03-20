f94a2cc99dab7f09362ad658c48f7c37e56b42d7
geflei <geflei@7c7787e4-0af9-0310-ac77-e5a7fa7f211a>
2006-03-20 17:53:07 +0000

    update spanish translation, adapt receivepopup.sh to inotify mask changes

--- trunk/klinpopup/src/receivepopup.sh
+++ trunk/klinpopup/src/receivepopup.sh
@@ -2,21 +2,12 @@
 RECEIVED=`date +"%s_%N"`
 POPUP_DIR=/var/lib/klinpopup
 POPUP_FILE=$POPUP_DIR/$RECEIVED
+# please DON'T change the time format here!
 TIME=`date --iso-8601=seconds`
 MESSAGE=`cat $4`
 
 # create message
-#sender
-echo -e "$1\n$2\n$3\n$TIME\n$MESSAGE" > /tmp/$RECEIVED
-mv /tmp/$RECEIVED $POPUP_DIR/
-#sender machine
-#echo $2 >> $POPUP_FILE
-#sender IP
-#echo $3 >> $POPUP_FILE
-# please DON'T change the time format here!
-#TIME=`date --iso-8601=seconds` >> $POPUP_FILE
-#message file
-#cat $4 >> $POPUP_FILE
+echo -e "$1\n$2\n$3\n$TIME\n$MESSAGE" > $POPUP_DIR/$RECEIVED
 
 # delete message file from samba
 rm -f $4
